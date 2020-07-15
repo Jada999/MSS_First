d5d4e0196df5b12d76fd82a775221f62e035001c
Jada999 <a2740335118@gmail.com>
2020-07-15 23:33:16 +1000

    Update test.py

--- test.py
+++ test.py
@@ -15,12 +15,19 @@ Please submit directly to “Genevievechen1996@hotmail.com”
 """
 
 # TODO: import xlwings libarary
-
+import xlwings as xw
+import pandas as pd
 
 # TODO: initialize sheets(Sheet name is “[your name] + "_answer"” for example "Bob_answer")
-
+wb = xw.Book()
+sheet = wb.sheets['Jada_answer']
 
 # TODO: finish Your Code
 
+df = pd.DataFrame([['Liam','Noah','William','James','Logan','Benjamin','Mason','Elijah','Oliver','Jacob'],[1,2,3,4,5,6,7,8,9,10]],
+                 index = ['name','student number'])
+
+sheet.range('Jada_answer').value= df
+sheet.range('Jada_answer').options(pd.DataFrame, expand='table').value
 
 #Today I am writing code with Serena and Jada
